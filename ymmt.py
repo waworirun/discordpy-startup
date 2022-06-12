0ae5ebdbe6ef0817ea123a624c1da6622a15b186
waworirun <107342169+waworirun@users.noreply.github.com>
2022-06-12 16:34:37 +0900

    Add files via upload

---
+++ ymmt.py
@@ -0,0 +1,28 @@
+import discord 
+
+client = discord.Client()
+
+TOKEN = 'OTg1NDM0NDEyNDMyOTAwMTU2.GIQ-o5.csauMLbnvWzEVcirBe2k7byX9AW-pvM5OIZJes'
+​
+client = discord.Client()
+@client.event
+async def on_ready():
+   
+   print('ログインしました')
+
+import random 
+   if message.content == "あ":
+　　　　　#↓
+       a = ["あ", "あ？", "分かる（分からない）", "い", "what", "E", "確かに"]
+       choice = random.choice(hennnahuri)＃←
+       await message.channel.send(choice)
+
+
+
+
+
+
+
+
+
+client.run(TOKEN)
